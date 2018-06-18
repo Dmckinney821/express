9b97a26c4e58e6b8ddaa15db5117fd8bfba017ca
Dan McKinney <3dhdaniel@gmail.com>
2018-06-18 11:32:37 -0400

    adds contacts database

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const contacts = require('./contacts');
 //Hone Page = show the user a welcome message
 app.get('/', (req, res) => {
     res.send('HAAAAAAAY');
 });
 // Contacts List Page = show the user all contacts
 app.get('/contacts', (req, res) => {
-    res.send("you are on the contacts page");
+    res.send(contacts.users);
 });
 // Contacts Detail = show the user all info for one contact
 app.get('/contacts/:id', (req, res) => {
-    res.send(`You are viewing details for ${req.params.id}`);
+    // res.send(`You are viewing details for ${req.params.id}`);
+    let id = req.params.id;
+    let contact = contacts.users.find((user) => {
+      return user.id === id;   
+    });
+    res.send(contact);
 });
 app.listen(8888, () => {
     console.log('Your express app is running at http://localhost:8888');
