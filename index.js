60076b911001a6164cd118853d7bb19c6fecc364
Dan McKinney <3dhdaniel@gmail.com>
2018-06-18 10:56:09 -0400

    contact

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('HAAAAAAAY');
+});
+app.listen(8888, () => {
+    console.log('Your express app is running at http://localhost:8888');
+});
